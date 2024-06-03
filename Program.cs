053e738d9de46a1d1d26fb570b60e76a12be114a
Jason De Lorme <delormej@gmail.com>
2024-06-03 17:36:22 -0400

    Adding authentication.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using Google.Apis.Auth.OAuth2;
+
 const string SERVICE_CLIENT = "ServiceClient";
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,9 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 string serviceUrl = builder.Configuration["ServiceUrl"] ?? 
     throw new Exception("ServiceUrl not found");
 
+string idToken = await GetIdTokenFromApplicationDefaultAsync(serviceUrl);
+
 builder.Services.AddHttpClient(SERVICE_CLIENT, client =>
 {
     client.BaseAddress = new Uri(serviceUrl);
+    client.DefaultRequestHeaders.Add("Authorization", $"Bearer {idToken}");
 });
 
 var app = builder.Build();
@@ -28,3 +32,22 @@ app.MapGet("/", async (IHttpClientFactory clientFactory) =>
 });
 
 app.Run();
+
+/// <summary>
+/// Gets an ID token from Application Default Credentials.
+/// </summary>
+static async Task<string> GetIdTokenFromApplicationDefaultAsync(string url, CancellationToken cancellationToken = default)
+{
+    // Get default Google credential
+    var credential = await GoogleCredential.GetApplicationDefaultAsync(cancellationToken)
+        .ConfigureAwait(false);
+
+    var token = await credential.GetOidcTokenAsync(OidcTokenOptions.FromTargetAudience(url), cancellationToken)
+        .ConfigureAwait(false);
+
+    // Despite the method being called AccessToken this is an IdToken
+    var idToken = await token.GetAccessTokenAsync(cancellationToken)
+        .ConfigureAwait(false);
+
+    return idToken;
+}
